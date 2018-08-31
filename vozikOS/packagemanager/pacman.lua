a8215a0d5ad5474dc4f4afa0bd6cfac185721079
petr.vozak <petr.vozak@PVO-PC.intern.seacomp.cz>
2018-08-31 21:29:47 +0200

    interface

---
+++ vozikOS/packagemanager/pacman.lua
@@ -0,0 +1,5 @@
+--[[
+
+]]
+
+local shell = require("shell")
--- vozikOS/src/main.lua
+++ vozikOS/src/main.lua
@@ -1,4 +1,11 @@
-local function main()
+local unicode = require("unicode")
 
+local function test(n, p, ...)
+  checkArg(1, n, "string")
+  print(...)
+end
+
+local function main()
+test(1, 2, 3)
 end
 main()
