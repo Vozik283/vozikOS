ef0c267d9d2ce42d80b0f09455442dba3fa81572
petr.vozak <petr.vozak@PVO-PC.intern.seacomp.cz>
2018-09-08 20:02:15 +0200

    user fix

--- vozikOS/mainserver/user.lua
+++ vozikOS/mainserver/user.lua
@@ -1,4 +1,5 @@
 local shell = require("shell")
+local component = require("component")
 local userapi = require("userapi")
 local userroles = require("userroles")
 local unicode = require("unicode")
